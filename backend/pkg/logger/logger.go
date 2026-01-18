bec8294127cfa584d8099f6f14674002a51795ef
ivan <otskok98@mail.ru>
2026-01-18 04:58:19 +0300

    mini-update with working docker-compose file and working just fine with grafana

--- backend/pkg/logger/logger.go
+++ backend/pkg/logger/logger.go
@@ -1,11 +1,21 @@
 package logger
 
 import (
+	"io"
 	"log/slog"
 	"os"
 )
 
 func NewLogger() *slog.Logger {
-	handler := slog.NewJSONHandler(os.Stdout, nil)
+	logFile, err := os.OpenFile("../logs_pet/app.json.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		handler := slog.NewJSONHandler(os.Stdout, nil)
+		return slog.New(handler)
+	}
+
+	// MultiWriter: пишем и в файл, и в stdout
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+
+	handler := slog.NewJSONHandler(multiWriter, nil)
 	return slog.New(handler)
 }
